b99d3fbab4e31d4e8e31a8f1dc93175aa187575b
Egor <egor.osokin7@gmail.com>
2021-09-24 17:49:23 +0300

    feat: add config

---
+++ popit.config.js
@@ -0,0 +1,14 @@
+// TODO: try for example project with different config
+
+module.exports = {
+  config: {},
+  navigation: {
+    example: "/example",
+    "example.login": "/example/login",
+  },
+  apps: {
+    example: {
+      version: "1.0.0",
+    },
+  },
+};
